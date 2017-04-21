bbcecb28e4cfbd5765cd7d454a83a765fbb765aa
Mentos <yuramitulya@mail.ru>
2017-04-21 08:49:52 +0300

    Задача владя

---
+++ zada4aSunuchka/zada4aSunuchka/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	double x;
+	cout << "Enter x: "; cin >> x;
+	x = x * 10;
+	int g = x;
+	cout << g % 10 << endl;
+	system("pause>>void");
+}
